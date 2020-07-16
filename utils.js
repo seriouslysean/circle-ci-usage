87d039f4a78097a998b9d2b6b9f91059d606ca34
Sean Kennedy <skennedy@urbn.com>
2020-07-16 10:52:21 -0400

    Add missing util

--- utils.js
+++ utils.js
@@ -10,8 +10,13 @@ function multiply(n1, n2) {
     return n1 * n2;
 }
 
+function divide(n1, n2) {
+    return n1 / n2;
+}
+
 module.exports = {
     add,
     subtract,
     multiply,
+    divide,
 };
